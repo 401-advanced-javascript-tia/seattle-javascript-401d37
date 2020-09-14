80a0d2657afa7823b000db73650c9fd4af88bd79
Tia Low <lowtia@gmail.com>
2020-09-14 10:18:06 -0700

    class 19 notes

--- class-19/demo/simple-queue/client.js
+++ class-19/demo/simple-queue/client.js
@@ -4,9 +4,9 @@ const client = require('socket.io-client');
 
 const socket = client.connect('http://localhost:3000');
 
-socket.emit('getall');
+// socket.emit('getall');
 
-socket.on('hello', message => {
-  console.log('heard', message.payload);
-  socket.emit('received', message);
+socket.on('hello-greeting', payload => {
+  console.log('heard', payload.message);
+  socket.emit('received', payload);
 });
--- class-19/demo/simple-queue/queue.js
+++ class-19/demo/simple-queue/queue.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const io = require('socket.io')(3000);
+// const uuid = require('uuid').v4();
 
 const queue = {
   hello: {}
@@ -8,20 +9,41 @@ const queue = {
 
 io.on('connection', socket => {
 
-  socket.on('hello', payload => {
+  console.log('connection made');
+
+  socket.on('hello', message => {
+
+    console.log('HALLO, msg received:', message);
+
     let id = Math.random();
-    queue.hello[id] = payload;
-    socket.broadcast.emit('hello', { id, payload });
+    queue.hello[id] = message;
+
+    const payload = {id, message};
+
+    // io.emit('hello-greeting', message);
+    console.log(queue.hello);
+    socket.broadcast.emit('hello-greeting', payload);
   });
 
   socket.on('getall', () => {
-    Object.keys(queue.hello).forEach(id => {
-      socket.emit('hello', { id, payload: queue.hello[id] });
-    })
+
+    for(let id in queue.hello) {
+
+      const message = queue.hello[id];
+      const payload = {id , message};
+
+      socket.emit('hello-greeting', payload);
+    }
+
+    // Object.keys(queue.hello).forEach(id => {
+    //   socket.emit('hello', { id, payload: queue.hello[id] });
+    // })
+
   });
 
   socket.on('received', message => {
     delete queue.hello[message.id];
+    console.log(queue.hello);
   });
 
 })
